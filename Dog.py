f869a499a2d46dd1d25184843ed88269982e6885
DELL <rajkumartamang182@gmail.com>
2022-06-28 13:12:30 +0545

    updated

---
+++ Dog.py
@@ -0,0 +1,10 @@
+class Dog:
+    def __init__(self, name, color, height, weight):
+        self.name=name 
+        self.color= color
+        self.height = height
+        self.weight = weight
+
+d= Dog("bruno", "white" , 20, 50)
+print(d.name);
+
