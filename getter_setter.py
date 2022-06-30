eb84a4b473c1c4fa0925552ad21e249b7af08517
DELL <rajkumartamang182@gmail.com>
2022-06-30 13:19:13 +0545

    Updated

---
+++ getter_setter.py
@@ -0,0 +1,24 @@
+class getter_setter:
+     def __init__(self):
+          self._age = 0
+       
+     # using property decorator
+     # a getter function
+     @property
+     def age(self):
+         print("getter method called")
+         return self._age
+       
+     # a setter function
+     @age.setter
+     def age(self, a):
+         if(a < 18):
+            raise ValueError("Sorry you age is below eligibility criteria")
+         print("setter method called")
+         self._age = a
+  
+mark = getter_setter()
+  
+mark.age = 19
+  
+print(mark.age)
